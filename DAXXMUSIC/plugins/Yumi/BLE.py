bcf82d0603c8dc2a0c5c0d9685ce8ba649c94672
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-22 21:26:19 +0300

    Update BLE.py

--- DAXXMUSIC/plugins/Yumi/BLE.py
+++ DAXXMUSIC/plugins/Yumi/BLE.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, idle
 from DAXXMUSIC import app
-
+from strings.filters import command
 from pyrogram import filters
 from pyrogram import Client
 
@@ -14,3 +14,18 @@ async def say(app, message):
     elif message.text.startswith("بڵێ"):
         txt = message.text.split(None, 1)[1]
         return await message.reply(txt)
+
+
+@app.on_message(command(["دل","dl","dll","دڵ"]))
+async def haert(c, msg):
+    for i in range(45):
+        await msg.edit(string_utils.shuffle("🩷❤️🧡💛💚🩵💙💜🖤🩶🤍🤎💔❤️‍🔥❤️‍🩹❣💕💞💓💗💖💘💝♥️‍"))
+        time.sleep(0.1)
+
+
+@app.on_message(command(["دلی","dlly","dli","دڵی"]))
+async def haerts(c, msg):
+    for i in range(1, 15):
+        await msg.edit(string_utils.shuffle(
+            "🩷❤️🧡💛💚🩵💙💜🖤🩶🤍🤎💔❤️‍🔥❤️‍🩹❣💕💞💓💗💖💘💝♥️🩷❤️🧡💛💚🩵💙💜🖤🩶🤍🤎💔❤️‍🔥❤️‍🩹❣💕💞💓💗💖💘💝♥️🩷❤️🧡💛💚🩵💙💜🖤🩶🤍🤎💔❤️‍🔥❤️‍🩹❣💕💞💓💗💖💘💝♥️‍"))
+        time.sleep(0.5)
