8b07514e59cf3e69958e669a6c00a63445a7b6cb
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-27 22:50:47 +0300

    Create tikdn.py

---
+++ DAXXMUSIC/plugins/tools/tikdn.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from DAXXMUSIC import app
+import requests
+
+
+# noinspection PyBroadException
+@app.on_message(filters.text & filters.private)
+async def Start(app, message):
+    try:
+        msg = message.text
+        url = requests.get(f'https://tikwm.com/api/?url={msg}').json()
+        music = url['data']['music']
+        region = url['data']['region']
+        tit = url['data']['title']
+        vid = url['data']['play']
+        ava = url['data']['author']['avatar']
+        ##
+        name = url['data']['music_info']['author']
+        time = url['data']['duration']
+        sh = url['data']['share_count']
+        com = url['data']['comment_count']
+        wat = url['data']['play_count']
+        await app.send_photo(message.chat.id, ava,
+                             caption=f'**✧ ¦ ناو : {name}\n✧ ¦ وڵات : {region}\n\n✧ ¦ ژمارەی بینەر : {wat}\n✧ ¦ ژمارەی کۆمێنت : {com}\n✧ ¦ ژمارەی شەیرەکان : {sh}\n✧ ¦ درێژی ڤیدیۆ : {time}**',
+                             )
+        await app.send_video(message.chat.id, vid, caption=f"{tit}")
+
+    except:
+        pass
