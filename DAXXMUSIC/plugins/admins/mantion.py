73898873f22d3bcc64efdc9cc8db392665d4ece5
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-30 01:07:29 +0300

    Update mantion.py

--- DAXXMUSIC/plugins/admins/mantion.py
+++ DAXXMUSIC/plugins/admins/mantion.py
@@ -6,19 +6,19 @@ from strings.filters import command
 SPAM_CHATS = []
 
 
-@app.on_message(command(["/mantion", "/all","تاگ","@all"]) & filters.group)
-async def tag_all_users(_,message): 
-    replied = message.reply_to_message  
+@app.on_message(command(["/mantion", "/all", "تاگ", "@all"]) & filters.group)
+async def tag_all_users(_, message):
+    replied = message.reply_to_message
     if len(message.command) < 2 and not replied:
-        await message.reply_text("**ڕیپلەی نامەیە یان شتێ بکە یان لەگەڵ فەرمان نامەیە بنووسە**") 
-        return                  
+        await message.reply_text("**ڕیپلەی نامەیە یان شتێ بکە یان لەگەڵ فەرمان نامەیە بنووسە**")
+        return
     if replied:
-        SPAM_CHATS.append(message.chat.id)      
-        usernum= 0
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
         usertxt = ""
-        async for m in app.get_chat_members(message.chat.id): 
+        async for m in app.get_chat_members(message.chat.id):
             if message.chat.id not in SPAM_CHATS:
-                break       
+                break
             usernum += 5
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 1:
@@ -26,41 +26,42 @@ async def tag_all_users(_,message):
                 await asyncio.sleep(2)
                 usernum = 0
                 usertxt = ""
-        try :
+        try:
             SPAM_CHATS.remove(message.chat.id)
         except Exception:
             pass
     else:
         text = message.text.split(None, 1)[1]
-        
+
         SPAM_CHATS.append(message.chat.id)
-        usernum= 0
+        usernum = 0
         usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):       
+        async for m in app.get_chat_members(message.chat.id):
             if message.chat.id not in SPAM_CHATS:
-                break 
+                break
             usernum += 1
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 5:
-                await app.send_message(message.chat.id,f'{text}\n{usertxt}')
+                await app.send_message(message.chat.id, f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
                 usernum = 0
-                usertxt = ""                          
-        try :
+                usertxt = ""
+        try:
             SPAM_CHATS.remove(message.chat.id)
         except Exception:
-            pass        
-           
-@app.on_message(command(["/cancel","وەستانی تاگ"]) & ~filters.private)
+            pass
+
+
+@app.on_message(command(["/cancel", "وەستانی تاگ"]) & ~filters.private)
 async def cancelcmd(_, message):
     chat_id = message.chat.id
     if chat_id in SPAM_CHATS:
-        try :
+        try:
             SPAM_CHATS.remove(chat_id)
         except Exception:
-            pass   
-        return await message.reply_text("**ᴛᴀɢ ᴀʟʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")     
-                                     
-    else :
-        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")  
+            pass
+        return await message.reply_text("**تاگ بە سەرکەوتوویی وەستا**")
+
+    else:
+        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
         return       
