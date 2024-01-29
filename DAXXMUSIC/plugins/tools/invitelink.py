ada8325386728da87c6ad0d0dab44baf2dc27ef2
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-29 18:55:33 +0300

    Update invitelink.py

--- DAXXMUSIC/plugins/tools/invitelink.py
+++ DAXXMUSIC/plugins/tools/invitelink.py
@@ -1,64 +1,14 @@
-from DAXXMUSIC import app
+import asyncio
 from pyrogram import Client, filters
-from pyrogram.errors import ChatIdInvalid
-from pyrogram.errors import ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant
-import os
-import json
+from strings.filters import command
 from pyrogram.types import Message
-from DAXXMUSIC.misc import SUDOERS
+from DAXXMUSIC import app
 
-
-@app.on_message(filters.command(["link", "invitelink"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
-async def link_command_handler(client: Client, message: Message):
-    if len(message.command) != 2:
-        await message.reply("Invalid usage. Correct format: /link group_id")
-        return
-
-    group_id = message.command[1]
-    file_name = f"group_info_{group_id}.txt"
-
+@app.on_message(command(["بەستەر","/link","لینک","لینك"]) & ~filters.bot & ~filters.private)
+async def invitelink(client, message):
+    chid = message.chat.id
     try:
-        chat = await client.get_chat(int(group_id))
-
-        if chat is None:
-            await message.reply("Unable to get information for the specified group ID.")
-            return
-
-        try:
-            invite_link = await client.export_chat_invite_link(chat.id)
-        except FloodWait as e:
-            await message.reply(f"FloodWait: {e.x} seconds. Retrying in {e.x} seconds.")
-            return
-
-        group_data = {
-            "id": chat.id,
-            "type": str(chat.type),
-            "title": chat.title,
-            "members_count": chat.members_count,
-            "description": chat.description,
-            "invite_link": invite_link,
-            "is_verified": chat.is_verified,
-            "is_restricted": chat.is_restricted,
-            "is_creator": chat.is_creator,
-            "is_scam": chat.is_scam,
-            "is_fake": chat.is_fake,
-            "dc_id": chat.dc_id,
-            "has_protected_content": chat.has_protected_content,
-        }
-
-        with open(file_name, "w", encoding="utf-8") as file:
-            for key, value in group_data.items():
-                file.write(f"{key}: {value}\n")
-
-        await client.send_document(
-            chat_id=message.chat.id,
-            document=file_name,
-            caption=f"𝘏𝘦𝘳𝘦 𝘐𝘴 𝘵𝘩𝘦 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘍𝘰𝘳\n{chat.title}\n𝘛𝘩𝘦 𝘎𝘳𝘰𝘶𝘱 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘚𝘤𝘳𝘢𝘱𝘦𝘥 𝘉𝘺 : @{app.username}"
-        )
-
-    except Exception as e:
-        await message.reply(f"Error: {str(e)}")
-
-    finally:
-        if os.path.exists(file_name):
-            os.remove(file_name)
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        return await message.reply_text("**سەرەتا بمکە ئەدمین**")
+    await message.reply_text(f"**بە سەرکەوتوویی بەستەری گرووپ دروست کرا :**\n\n `{invitelink}`")
