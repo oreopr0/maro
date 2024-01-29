73913b8becba39a36c69b7af24ee94e9b7201268
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-29 13:41:31 +0300

    Update alina.py

--- DAXXMUSIC/plugins/play/alina.py
+++ DAXXMUSIC/plugins/play/alina.py
@@ -1,6 +1,5 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
 from strings.filters import command
 from DAXXMUSIC.utils.decorators import AdminActual
 from pyrogram.types import (
