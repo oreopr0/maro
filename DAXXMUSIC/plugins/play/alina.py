e830694411014e78437a7234eed2202d608e2db0
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-31 14:37:29 +0300

    Update alina.py

--- DAXXMUSIC/plugins/play/alina.py
+++ DAXXMUSIC/plugins/play/alina.py
@@ -87,28 +87,9 @@ async def delet(client: Client, message: Message):
     )
 
 
-@app.on_message(filters.command("ميرا نادي المطور", [".", ""]) & filters.group)
-async def kstr(client: Client, message: Message):
-       chat = message.chat.id
-       gti = message.chat.title
-       link = await app.export_chat_invite_link(chat)
-       usr = await client.get_users(message.from_user.id)
-       chatusername = f"@{message.chat.username}"
-       user_id = message.from_user.id
-       user_ab = message.from_user.username
-       user_name = message.from_user.first_name
-       buttons = [[InlineKeyboardButton(gti, url=f"{link}")]]
-       reply_markup = InlineKeyboardMarkup(buttons)
-       
-       await app.send_message(-1001801583479, f"- قام {message.from_user.mention}\n- بمناداتك عزيزي المطور\n- ايديه {user_id}\n- يوزره @{user_ab}\n- ايدي القروب {message.chat.id}\n- يوزر القروب {chatusername}",
-       reply_markup=reply_markup,
-       )
-       await message.reply_text(
-        f"""- **ابشر ياعيوني ارسلت للمطور بيخش القروب ويشوف مشكلتك بأقرب وقت\n\n- تابع قناة البوت عشات تشوف التحديثات** -› [• Source Mira •](t.me/NvvvC)""", disable_web_page_preview=True     
-    )
 
 
-REPLY_MESSAGE = "**• بەخێربێی ئەزیزم کۆنتڕۆڵکردنی بە دوگمەکانی خوارەوەیە"
+REPLY_MESSAGE = "**• بەخێربێی ئەزیزم کۆنتڕۆڵکردنی بە دوگمەکانی خوارەوەیە**"
 
 
 
