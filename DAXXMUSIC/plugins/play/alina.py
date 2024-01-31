463b1004abc7a56dfa5b85ac92a27bc0131880c9
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2024-01-31 14:08:11 +0300

    Update alina.py

--- DAXXMUSIC/plugins/play/alina.py
+++ DAXXMUSIC/plugins/play/alina.py
@@ -3,6 +3,7 @@ from pyrogram import Client, filters
 from strings.filters import command
 from DAXXMUSIC.utils.decorators import AdminActual
 from pyrogram.types import (
+    CallbackQuery,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     ReplyKeyboardMarkup,
