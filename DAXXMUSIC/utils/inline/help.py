567c9c791e4f2bbf15e0e986474a3d719205004e
z75z <110237161+z75z@users.noreply.github.com>
2024-02-01 14:31:15 +0300

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -10,7 +10,7 @@ def help_pannel(_, START: Union[bool, int] = None):
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            url=f"https://t.me/{app.username}?start=help",
+            url=f"https://t.me/{app.username}?start",
         ),
     ]
     mark = second if START else first
