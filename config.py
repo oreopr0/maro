cf25e7d50b99d45569467de00c9a56eb596c7c5a
z75z <110237161+z75z@users.noreply.github.com>
2024-02-01 14:30:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zzzdevo/Z07",
+    "https://github.com/z75z/Z07",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
