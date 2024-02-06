23275017be753320d0afd31d50c16ab003c7f43a
oreopr0 <123789155+oreopr0@users.noreply.github.com>
2024-02-06 04:36:25 +0300

    Update BLE.py

--- DAXXMUSIC/plugins/Yumi/BLE.py
+++ DAXXMUSIC/plugins/Yumi/BLE.py
@@ -6,9 +6,9 @@ from pyrogram import Client
 
 
 
-@app.on_message(filters.regex("^بڵێ|^بلی") & filters.group)
+@app.on_message(filters.regex("^قول|^بلی") & filters.group)
 async def say(app, message):
-    if message.text.startswith("بلی") and message.reply_to_message:
+    if message.text.startswith("قول") and message.reply_to_message:
         txt = message.text.split(None, 1)[1]
         return await message.reply_to_message.reply(txt)
 
